b5e53aa101e6e96f977d4227d67516f190ce02db
matsumo <matsumo@ce.ns0.it>
2019-11-20 22:45:02 +0900

    ハードウエアスクロールはあきらめて、ソフトウエア制御に書き直し

--- TFT_Terminal.ino
+++ TFT_Terminal.ino
@@ -25,35 +25,21 @@
 
 #include <M5StickC.h>
 
-// The scrolling area must be a integral multiple of TEXT_HEIGHT
+#define TEXT_WIDTH 6 // Width of text to be printed and scrolled
 #define TEXT_HEIGHT 8 // Height of text to be printed and scrolled
-#define TOP_FIXED_AREA 0 // Number of lines in top fixed area (lines counted from top of screen)
-#define BOT_FIXED_AREA 0 // Number of lines in bottom fixed area (lines counted from bottom of screen)
 #define YMAX 80 // Bottom of screen area
 #define XMAX 156
 
-// The initial y coordinate of the top of the scrolling area
-uint16_t yStart = 0;
-// yArea must be a integral multiple of TEXT_HEIGHT
-uint16_t yArea = YMAX-TOP_FIXED_AREA-BOT_FIXED_AREA;
-// The initial y coordinate of the top of the bottom text line
-// uint16_t yDraw = YMAX - BOT_FIXED_AREA - TEXT_HEIGHT;
+#define YCHAR (YMAX / TEXT_HEIGHT)
+#define XCHAR (XMAX / TEXT_WIDTH)
+
+unsigned char vram[YCHAR][XCHAR];
+
 uint16_t yDraw = 0;
 
 // Keep track of the drawing x coordinate
 uint16_t xPos = 0;
 
-// For the byte we read from the serial port
-byte data = 0;
-
-// A few test variables used during debugging
-boolean change_colour = 1;
-boolean selected = 1;
-
-// We have to blank the top line each time the display is scrolled, but this takes up to 13 milliseconds
-// for a full width line, meanwhile the serial buffer may be filling... and overflowing
-// We can speed up scrolling of short text lines by just blanking the character we drew
-int blank[19]; // We keep all the strings pixel lengths to optimise the speed of the top line blanking
 
 void termInit() {
   // Setup the TFT display
@@ -73,11 +59,8 @@ void termInit() {
   // Change colour for scrolling zone text
   M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
 
-  // Setup scroll area
-  setupScrollArea(TOP_FIXED_AREA, BOT_FIXED_AREA);
-
   // Zero the array
-  // for (byte i = 0; i<18; i++) blank[i]=0;
+  memset(vram, XCHAR * YCHAR, 0);
 }
 
 
@@ -96,16 +79,17 @@ void termPutchar(unsigned char data) {
   {
     // data = Serial.read();
     // If it is a CR or we are near end of line then scroll one line
-    if (data == '\r' || xPos>=XMAX) {
+    if (data == '\r' || xPos>=XCHAR) {
       xPos = 0;
       yDraw = scroll_line(); // It can take 13ms to scroll and blank 16 pixel lines
     }
-    if(data == '\b' && xPos > 6){
-      xPos -= 6;
+    if(data == '\b' && xPos > 0){
+      xPos--;
     }
     if (data > 31 && data < 128) {
-      xPos += M5.Lcd.drawChar(data,xPos,yDraw);
-      // blank[(18+(yStart-TOP_FIXED_AREA)/TEXT_HEIGHT)%19]=xPos; // Keep a record of line lengths
+      vram[yDraw][xPos] = data;
+      M5.Lcd.drawChar(data, xPos * TEXT_WIDTH, yDraw * TEXT_HEIGHT);
+      xPos ++;
     }
     //change_colour = 1; // Line to indicate buffer is being emptied
   }
@@ -120,40 +104,17 @@ void printString(unsigned char *str)
 // Call this function to scroll the display one text line
 // ##############################################################################################
 int scroll_line() {
-  int yTemp = yStart; // Store the old yStart, this is where we draw the next line
-  // Use the record of line lengths to optimise the rectangle size we need to erase the top line
-  // M5.Lcd.fillRect(0,yStart,blank[(yStart-TOP_FIXED_AREA)/TEXT_HEIGHT],TEXT_HEIGHT, TFT_BLACK);
-  M5.Lcd.fillRect(0,yStart,XMAX,TEXT_HEIGHT, TFT_BLACK);
-
-  // Change the top of the scroll area
-  yStart+=TEXT_HEIGHT;
-  // The value must wrap around as the screen memory is a circular buffer
-  // if (yStart >= YMAX - BOT_FIXED_AREA) yStart = TOP_FIXED_AREA + (yStart - YMAX + BOT_FIXED_AREA);
-  if (yStart >= YMAX) yStart = 0;
-  // Now we can scroll the display
-  scrollAddress(yStart);
-  return  yTemp;
-}
-
-// ##############################################################################################
-// Setup a portion of the screen for vertical scrolling
-// ##############################################################################################
-// We are using a hardware feature of the display, so we can only scroll in portrait orientation
-void setupScrollArea(uint16_t tfa, uint16_t bfa) {
-  M5.Lcd.writecommand(ST7735_VSCRDEF); // Vertical scroll definition
-  M5.Lcd.writedata(tfa >> 8);           // Top Fixed Area line count
-  M5.Lcd.writedata(tfa);
-  M5.Lcd.writedata((YMAX-tfa-bfa)>>8);  // Vertical Scrolling Area line count
-  M5.Lcd.writedata(YMAX-tfa-bfa);
-  M5.Lcd.writedata(bfa >> 8);           // Bottom Fixed Area line count
-  M5.Lcd.writedata(bfa);
-}
-
-// ##############################################################################################
-// Setup the vertical scrolling start address pointer
-// ##############################################################################################
-void scrollAddress(uint16_t vsp) {
-/*  M5.Lcd.writecommand(ST7735_VSCRSADD); // Vertical scrolling start address
-  M5.Lcd.writedata(vsp>>8);
-  M5.Lcd.writedata(vsp);*/
+  if(yDraw < (YCHAR-1)){
+    return yDraw+1;
+  }else{
+    memcpy(vram, &vram[1][0], XCHAR * (YCHAR - 1));
+    memset(&vram[YCHAR - 1][0], 0, XCHAR);
+    M5.Lcd.fillScreen(TFT_BLACK);
+    for(int y=0; y<YCHAR; y++){
+      for(int x=0; x<XCHAR; x++){
+        if(vram[y][x] > 31 && vram[y][x] < 128) M5.Lcd.drawChar(vram[y][x], x * TEXT_WIDTH, y * TEXT_HEIGHT);
+      }
+    }
+    return YCHAR-1;
+  }
 }
